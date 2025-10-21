587e992edc4ea6b25e4a69faaade01a5d60b6c2f
Kingsley530 <115776273+Kingsley530@users.noreply.github.com>
2025-10-21 18:23:39 +0800

    Add files via upload

---
+++ extra.py
@@ -0,0 +1,311 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+from torch.jit import Final
+import math
+import numpy as np
+from functools import partial
+from typing import Optional, Callable, Union
+from einops import rearrange, reduce
+from ..modules.conv import Conv, DWConv, DSConv, RepConv, GhostConv, autopad
+from ..modules.block import *
+from .attention import *
+from .rep_block import *
+from .kernel_warehouse import KWConv
+from .dynamic_snake_conv import DySnakeConv
+from .ops_dcnv3.modules import DCNv3, DCNv3_DyHead
+from .shiftwise_conv import ReparamLargeKernelConv
+from .mamba_vss import *
+from .fadc import AdaptiveDilatedConv
+from .hcfnet import PPA, LocalGlobalAttention
+from ..backbone.repvit import Conv2d_BN, RepVGGDW, SqueezeExcite
+from ..backbone.rmt import RetBlock, RelPos2d
+from ..backbone.inceptionnext import InceptionDWConv2d, MetaNeXtBlock
+from .kan_convs import FastKANConv2DLayer, KANConv2DLayer, KALNConv2DLayer, KACNConv2DLayer, KAGNConv2DLayer
+from .deconv import DEConv
+from .SMPConv import SMPConv
+from .camixer import CAMixer
+from .orepa import *
+from .RFAconv import *
+from .wtconv2d import *
+from .metaformer import *
+from .tsdn import DTAB, LayerNorm
+from .ast import FRFN2D
+from .savss import SAVSS_Layer
+from .mobileMamba.mobilemamba import MobileMambaBlock
+from ..backbone.MambaOut import GatedCNNBlock_BCHW
+from .transformer import DynamicTanh
+from .efficientvim import *
+from .filc import *
+from .DCMPNet import *
+from ..backbone.overlock import RepConvBlock
+
+from ultralytics.utils.ops import make_divisible
+from timm.layers import CondConv2d, trunc_normal_, use_fused_attn, to_2tuple
+from timm.models import named_apply
+
+__all__ = ['C3k2_DCMB', 'SBA', 'HAFB', 'Detect_LSDECD'
+           ]
+
+######################################## DynamicConvMixerBlock start ########################################
+
+class DynamicInceptionDWConv2d(nn.Module):
+    """ Dynamic Inception depthweise convolution
+    """
+    def __init__(self, in_channels, square_kernel_size=3, band_kernel_size=11):
+        super().__init__()
+        self.dwconv = nn.ModuleList([
+            nn.Conv2d(in_channels, in_channels, square_kernel_size, padding=square_kernel_size//2, groups=in_channels),
+            nn.Conv2d(in_channels, in_channels, kernel_size=(1, band_kernel_size), padding=(0, band_kernel_size//2), groups=in_channels),
+            nn.Conv2d(in_channels, in_channels, kernel_size=(band_kernel_size, 1), padding=(band_kernel_size//2, 0), groups=in_channels)
+        ])
+        
+        self.bn = nn.BatchNorm2d(in_channels)
+        self.act = nn.SiLU()
+        
+        # Dynamic Kernel Weights
+        self.dkw = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, in_channels * 3, 1)
+        )
+        
+    def forward(self, x):
+        x_dkw = rearrange(self.dkw(x), 'bs (g ch) h w -> g bs ch h w', g=3)
+        x_dkw = F.softmax(x_dkw, dim=0)
+        x = torch.stack([self.dwconv[i](x) * x_dkw[i] for i in range(len(self.dwconv))]).sum(0)
+        return self.act(self.bn(x))
+
+class DynamicInceptionMixer(nn.Module):
+    def __init__(self, channel=256, kernels=[3, 5]):
+        super().__init__()
+        self.groups = len(kernels)
+        min_ch = channel // 2
+        
+        self.convs = nn.ModuleList([])
+        for ks in kernels:
+            self.convs.append(DynamicInceptionDWConv2d(min_ch, ks, ks * 3 + 2))
+        self.conv_1x1 = Conv(channel, channel, k=1)
+        
+    def forward(self, x):
+        _, c, _, _ = x.size()
+        x_group = torch.split(x, [c // 2, c // 2], dim=1)
+        x_group = torch.cat([self.convs[i](x_group[i]) for i in range(len(self.convs))], dim=1)
+        x = self.conv_1x1(x_group)
+        return x
+
+class DynamicIncMixerBlock(nn.Module):
+    def __init__(self, dim, drop_path=0.0):
+        super().__init__()
+        self.norm1 = nn.BatchNorm2d(dim)
+        self.norm2 = nn.BatchNorm2d(dim)
+        self.mixer = DynamicInceptionMixer(dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.mlp = ConvolutionalGLU(dim)
+        layer_scale_init_value = 1e-2            
+        self.layer_scale_1 = nn.Parameter(
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+        self.layer_scale_2 = nn.Parameter(
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+
+    def forward(self, x):
+        x = x + self.drop_path(self.layer_scale_1.unsqueeze(-1).unsqueeze(-1) * self.mixer(self.norm1(x)))
+        x = x + self.drop_path(self.layer_scale_2.unsqueeze(-1).unsqueeze(-1) * self.mlp(self.norm2(x)))
+        return x
+
+class C3k_DCMB(C3k):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5, k=3):
+        super().__init__(c1, c2, n, shortcut, g, e, k)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(DynamicIncMixerBlock(c_) for _ in range(n)))
+
+class C3k2_DCMB(C3k2):
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        super().__init__(c1, c2, n, c3k, e, g, shortcut)
+        self.m = nn.ModuleList(C3k_DCMB(self.c, self.c, 2, shortcut, g) if c3k else DynamicIncMixerBlock(self.c) for _ in range(n))
+        
+class DynamicCIncMixerBlock_KAN(nn.Module):
+    def __init__(self, dim, drop_path=0.0):
+        super().__init__()
+        self.norm1 = nn.BatchNorm2d(dim)
+        self.norm2 = nn.BatchNorm2d(dim)
+        self.mixer = DynamicIncMixerBlock(dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.mlp = KAN(dim, hidden_features=int(dim * 0.5))
+        layer_scale_init_value = 1e-2            
+        self.layer_scale_1 = nn.Parameter(
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+        self.layer_scale_2 = nn.Parameter(
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+
+    def forward(self, x):
+        N, C, H, W = x.size()
+        x = x + self.drop_path(self.layer_scale_1.unsqueeze(-1).unsqueeze(-1) * self.mixer(self.norm1(x)))
+        x = x + self.drop_path(self.layer_scale_2.unsqueeze(-1).unsqueeze(-1) * self.mlp(self.norm2(x).flatten(2).permute(0, 2, 1)).permute(0, 2, 1).view([-1, C, H, W]).contiguous())
+        return x
+
+class C3k_DCMB_KAN(C3k):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5, k=3):
+        super().__init__(c1, c2, n, shortcut, g, e, k)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(DynamicCIncMixerBlock_KAN(c_) for _ in range(n)))
+
+class C3k2_DCMB_KAN(C3k2):
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        super().__init__(c1, c2, n, c3k, e, g, shortcut)
+        self.m = nn.ModuleList(C3k_DCMB_KAN(self.c, self.c, 2, shortcut, g) if c3k else DynamicCIncMixerBlock_KAN(self.c) for _ in range(n))
+
+######################################## DynamicConvMixerBlock end ########################################
+
+######################################## Re-CalibrationFPN end ########################################
+
+def Upsample(x, size, align_corners = False):
+    """
+    Wrapper Around the Upsample Call
+    """
+    return nn.functional.interpolate(x, size=size, mode='bilinear', align_corners=align_corners)
+
+class SBA(nn.Module):
+
+    def __init__(self, inc, input_dim=64):
+        super().__init__()
+
+        self.input_dim = input_dim
+
+        self.d_in1 = Conv(input_dim//2, input_dim//2, 1)
+        self.d_in2 = Conv(input_dim//2, input_dim//2, 1)       
+                
+        self.conv = Conv(input_dim, input_dim, 3)
+        self.fc1 = nn.Conv2d(inc[1], input_dim//2, kernel_size=1, bias=False)
+        self.fc2 = nn.Conv2d(inc[0], input_dim//2, kernel_size=1, bias=False)
+        
+        self.Sigmoid = nn.Sigmoid()
+        
+    def forward(self, x):
+        H_feature, L_feature = x
+
+        L_feature = self.fc1(L_feature)
+        H_feature = self.fc2(H_feature)
+        
+        g_L_feature =  self.Sigmoid(L_feature)
+        g_H_feature = self.Sigmoid(H_feature)
+        
+        L_feature = self.d_in1(L_feature)
+        H_feature = self.d_in2(H_feature)
+
+        L_feature = L_feature + L_feature * g_L_feature + (1 - g_L_feature) * Upsample(g_H_feature * H_feature, size= L_feature.size()[2:], align_corners=False)
+        H_feature = H_feature + H_feature * g_H_feature + (1 - g_H_feature) * Upsample(g_L_feature * L_feature, size= H_feature.size()[2:], align_corners=False) 
+        
+        H_feature = Upsample(H_feature, size = L_feature.size()[2:])
+        out = self.conv(torch.cat([H_feature, L_feature], dim=1))
+        return out
+
+######################################## Re-CalibrationFPN end ########################################
+
+######################################## Hierarchical Attention Fusion Block start ########################################
+
+class HAFB(nn.Module):
+    # Hierarchical Attention Fusion Block
+    def __init__(self, inc, ouc, group=False):
+        super(HAFB, self).__init__()
+        ch_1, ch_2 = inc
+        hidc = ouc // 2
+
+        self.lgb1_local = LocalGlobalAttention(hidc, 2)
+        self.lgb1_global = LocalGlobalAttention(hidc, 4)
+        self.lgb2_local = LocalGlobalAttention(hidc, 2)
+        self.lgb2_global = LocalGlobalAttention(hidc, 4)
+
+        self.W_x1 = Conv(ch_1, hidc, 1, act=False)
+        self.W_x2 = Conv(ch_2, hidc, 1, act=False)
+        self.W = Conv(hidc, ouc, 3, g=4)
+
+        self.conv_squeeze = Conv(ouc * 3, ouc, 1)
+        self.rep_conv = RepConv(ouc, ouc, 3, g=(16 if group else 1))
+        self.conv_final = Conv(ouc, ouc, 1)
+
+    def forward(self, inputs):
+        x1, x2 = inputs
+        W_x1 = self.W_x1(x1)
+        W_x2 = self.W_x2(x2)
+        bp = self.W(W_x1 + W_x2)
+
+        x1 = torch.cat([self.lgb1_local(W_x1), self.lgb1_global(W_x1)], dim=1)
+        x2 = torch.cat([self.lgb2_local(W_x2), self.lgb2_global(W_x2)], dim=1)
+
+        return self.conv_final(self.rep_conv(self.conv_squeeze(torch.cat([x1, x2, bp], 1))))
+
+######################################## Hierarchical Attention Fusion Block end ########################################
+
+class Detect_LSDECD(nn.Module):
+    # Lightweight Shared Detail Enhanced Convolutional Detection Head
+    """YOLOv8 Detect head for detection models."""
+
+    dynamic = False  # force grid reconstruction
+    export = False  # export mode
+    shape = None
+    anchors = torch.empty(0)  # init
+    strides = torch.empty(0)  # init
+
+    def __init__(self, nc=80, hidc=256, ch=()):
+        """Initializes the YOLOv8 detection layer with specified number of classes and channels."""
+        super().__init__()
+        self.nc = nc  # number of classes
+        self.nl = len(ch)  # number of detection layers
+        self.reg_max = 16  # DFL channels (ch[0] // 16 to scale 4/8/12/16/20 for n/s/m/l/x)
+        self.no = nc + self.reg_max * 4  # number of outputs per anchor
+        self.stride = torch.zeros(self.nl)  # strides computed during build
+        self.conv = nn.ModuleList(nn.Sequential(Conv_GN(x, hidc, 1)) for x in ch)
+        self.share_conv = nn.Sequential(DEConv_GN(hidc), DEConv_GN(hidc))
+        self.cv2 = nn.Conv2d(hidc, 4 * self.reg_max, 1)
+        self.cv3 = nn.Conv2d(hidc, self.nc, 1)
+        self.scale = nn.ModuleList(Scale(1.0) for x in ch)
+        self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
+
+    def forward(self, x):
+        """Concatenates and returns predicted bounding boxes and class probabilities."""
+        for i in range(self.nl):
+            x[i] = self.conv[i](x[i])
+            x[i] = self.share_conv(x[i])
+            x[i] = torch.cat((self.scale[i](self.cv2(x[i])), self.cv3(x[i])), 1)
+        if self.training:  # Training path
+            return x
+
+        # Inference path
+        shape = x[0].shape  # BCHW
+        x_cat = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2)
+        if self.dynamic or self.shape != shape:
+            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
+            self.shape = shape
+
+        if self.export and self.format in ("saved_model", "pb", "tflite", "edgetpu", "tfjs"):  # avoid TF FlexSplitV ops
+            box = x_cat[:, : self.reg_max * 4]
+            cls = x_cat[:, self.reg_max * 4 :]
+        else:
+            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
+        dbox = self.decode_bboxes(box)
+
+        if self.export and self.format in ("tflite", "edgetpu"):
+            # Precompute normalization factor to increase numerical stability
+            # See https://github.com/ultralytics/ultralytics/issues/7371
+            img_h = shape[2]
+            img_w = shape[3]
+            img_size = torch.tensor([img_w, img_h, img_w, img_h], device=box.device).reshape(1, 4, 1)
+            norm = self.strides / (self.stride[0] * img_size)
+            dbox = dist2bbox(self.dfl(box) * norm, self.anchors.unsqueeze(0) * norm[:, :2], xywh=True, dim=1)
+
+        y = torch.cat((dbox, cls.sigmoid()), 1)
+        return y if self.export else (y, x)
+
+    def bias_init(self):
+        """Initialize Detect() biases, WARNING: requires stride availability."""
+        m = self  # self.model[-1]  # Detect() module
+        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
+        # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
+        # for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
+        m.cv2.bias.data[:] = 1.0  # box
+        m.cv3.bias.data[: m.nc] = math.log(5 / m.nc / (640 / 16) ** 2)  # cls (.01 objects, 80 classes, 640 img)
+
+    def decode_bboxes(self, bboxes):
+        """Decode bounding boxes."""
+        return dist2bbox(self.dfl(bboxes), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
